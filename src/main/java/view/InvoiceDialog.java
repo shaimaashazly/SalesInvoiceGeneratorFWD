64982302819612058169eec690914201ae271384
shaimaashazly <112861431+shaimaashazly@users.noreply.github.com>
2022-10-04 11:48:03 +0200

    sales invoice generator maven project for professional track made by shaimaa youssef

---
+++ src/main/java/view/InvoiceDialog.java
@@ -0,0 +1,96 @@
+package view;
+
+import controller.InvoiceLisener;
+import model.InvoiceHeader;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.event.*;
+
+public class InvoiceDialog extends JDialog {
+    private JPanel contentPane;
+    private JButton buttonOK;
+    private JButton buttonCancel;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+
+    private MainFrame mainFrame;
+
+    public InvoiceDialog(MainFrame mainFrame) {
+
+
+        getRootPane().setDefaultButton(buttonOK);
+
+        setContentPane(contentPane);
+        setModal(true);
+
+        this.mainFrame = mainFrame;
+
+        buttonOK.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onOK();
+            }
+        });
+
+        buttonCancel.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onCancel();
+            }
+        });
+
+        // call onCancel() when cross is clicked
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                onCancel();
+            }
+        });
+
+        // call onCancel() on ESCAPE
+        contentPane.registerKeyboardAction(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onCancel();
+            }
+        }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+    }
+
+    private void onOK() {
+        // add your code here
+        InvoiceHeader invoiceHeader = new InvoiceHeader();
+        invoiceHeader.setInvoiceDate(InvoiceLisener.validateDate(textField1.getText(), mainFrame));
+        invoiceHeader.setCustomerName(textField2.getText());
+        invoiceHeader.setInvoiceTotal(Double.parseDouble(textField3.getText()));
+        int lastId = 0;
+        for (int i = 0; i < mainFrame.invoicesList.size(); i++) {
+            InvoiceHeader inv = mainFrame.invoicesList.get(i);
+            if (inv.getInvoiceNum() > lastId)
+                lastId = inv.getInvoiceNum();
+        }
+        invoiceHeader.setInvoiceNum(lastId + 1);
+        System.out.println("ID : " + invoiceHeader.getInvoiceNum() + " Date :" + invoiceHeader.getInvoiceDate() + "  Name : " + invoiceHeader.getCustomerName() + " total : " + invoiceHeader.getInvoiceTotal());
+        mainFrame.invoicesList.add(invoiceHeader);
+        DefaultTableModel tableModel = JTableHelper.constructInvoicesModel(mainFrame.invoicesList);
+        mainFrame.invoicesTable.setModel(tableModel);
+        mainFrame.invoicesTable.repaint();
+
+        dispose();
+
+        InvoiceItemDialog dialog = new InvoiceItemDialog(mainFrame, invoiceHeader);
+        dialog.pack();
+        dialog.setVisible(true);
+
+    }
+
+    private void onCancel() {
+        // add your code here if necessary
+        dispose();
+    }
+
+    public static void main(String[] args) {
+        InvoiceDialog dialog = new InvoiceDialog(new MainFrame());
+        dialog.pack();
+        dialog.setVisible(true);
+        System.exit(0);
+    }
+}
---
+++ src/main/java/view/MainFrame.java
@@ -0,0 +1,235 @@
+package view;
+
+import controller.InvoiceItemsHelper;
+import controller.InvoiceLisener;
+import model.FileOperations;
+import model.InvoiceHeader;
+import model.InvoiceLine;
+
+import javax.swing.*;
+import javax.swing.border.TitledBorder;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class MainFrame extends JFrame {
+
+    public ArrayList<InvoiceHeader> invoicesList;
+    public ArrayList<InvoiceLine> invoiceItemsList = new ArrayList<>();
+    public String invoiceFilePath;
+    public String invoiceItemsFilePath;
+
+
+    private JPanel leftPanel;
+    private JPanel rightPanel;
+    private JPanel mainPanel;
+
+
+    public JTable invoicesTable;
+    public JTable invoiceItemsTable;
+    DefaultTableModel defaultTableModel;
+
+    public JLabel invNumTF;
+    public JTextField invDateTF;
+    public JTextField customerNmTF;
+    public JLabel invTotalTF;
+    public JButton saveButton;
+    public JButton cancelButton;
+
+    public MainFrame() {
+        super("Design Preview [New JFrame]");
+
+        invoiceFilePath = "InvoiceHeader.csv";
+        invoiceItemsFilePath = "InvoiceLine.csv";
+
+
+        fillInvoicesList();
+        fillInvoiceItemsListFromFile(this.invoiceItemsFilePath);
+
+        addMeu();
+        initMainPanel();
+        add(mainPanel);
+
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+    }
+
+
+    private void addMeu() {
+        JMenuBar invoiceMenuBar = new JMenuBar();
+        InvoiceMenu invoiceMenu = new InvoiceMenu(this);
+        invoiceMenuBar.add(invoiceMenu.getInvoiceFileMenu());
+        setJMenuBar(invoiceMenuBar);
+    }
+
+    private void initMainPanel() {
+        mainPanel = new JPanel();
+        mainPanel.setLayout(new GridLayout(1, 2));
+
+        initLeftPanel();
+        mainPanel.add(leftPanel);
+
+        InvoiceHeader invoiceHeader = invoicesList.get(0);
+        initRightPanel(invoiceHeader);
+        mainPanel.add(rightPanel);
+
+    }
+
+    private void initLeftPanel() {
+        leftPanel = new JPanel();
+        leftPanel.setLayout(new GridLayout(2, 1));
+        JPanel tablePanel = new JPanel();
+        JPanel buttonsPanel = new JPanel();
+
+        JButton createButton = new JButton("Create New Invoice");
+        createButton.setActionCommand("N");
+        createButton.addActionListener(new InvoiceLisener(this));
+
+        JButton deleteButton = new JButton("Delete Invoice");
+        deleteButton.setActionCommand("D");
+        deleteButton.addActionListener(new InvoiceLisener(this));
+
+        tablePanel.setBorder(BorderFactory.createTitledBorder(
+                BorderFactory.createEtchedBorder(), "Invoices Table", TitledBorder.LEFT,
+                TitledBorder.TOP));
+
+
+        fillInvoicesList();
+
+        invoicesTable = JTableHelper.loadInvoicesTable(this.invoicesList);
+
+        JScrollPane jScrollPane = new JScrollPane(invoicesTable);
+        jScrollPane.setPreferredSize(new Dimension(500, 300));
+        invoicesTable.setDefaultEditor(Object.class, null);
+        invoicesTable.getSelectionModel().addListSelectionListener(new InvoiceLisener(this));
+
+
+        tablePanel.add(jScrollPane);
+
+        buttonsPanel.add(createButton);
+        buttonsPanel.add(deleteButton);
+
+
+        leftPanel.add(tablePanel);
+        leftPanel.add(buttonsPanel);
+    }
+
+    public void fillInvoicesList() {
+        FileOperations fileOperations = new FileOperations(this);
+        this.invoicesList = fileOperations.readInvoiceFile(invoiceFilePath);
+    }
+
+    public void fillInvoiceItemsListFromFile(String filePath) {
+        this.invoiceItemsFilePath = filePath;
+        this.invoiceItemsList = new FileOperations(this).readInvoiceLineFile(invoiceItemsFilePath);
+
+    }
+
+
+    private void initRightPanel(InvoiceHeader invoiceHeader) {
+        rightPanel = new JPanel();
+        rightPanel.setLayout(new GridLayout(3, 1));
+        JPanel infoPanel = new JPanel(new GridLayout(4, 2));// first panel
+        JPanel tablePanel = new JPanel();  // second panel
+        JPanel buttonsPanel = new JPanel();  // third panel
+
+
+        invNumTF = new JLabel();
+        invDateTF = new JTextField();
+        customerNmTF = new JTextField();
+        invTotalTF = new JLabel();
+
+        infoPanel.add(new JLabel("Invoice Number : "));
+        infoPanel.add(invNumTF);
+        infoPanel.add(new JLabel("Invoice Date : "));
+        infoPanel.add(invDateTF);
+        infoPanel.add(new JLabel("Customer Name : "));
+        infoPanel.add(customerNmTF);
+        infoPanel.add(new JLabel("Invoice Total : "));
+        infoPanel.add(invTotalTF);
+
+
+        tablePanel.setBorder(BorderFactory.createTitledBorder(
+                BorderFactory.createEtchedBorder(), "Invoice Items", TitledBorder.LEFT,
+                TitledBorder.TOP));
+
+
+        ArrayList<InvoiceLine> invoiceLines = new InvoiceItemsHelper(invoiceItemsFilePath).getInvoiceItems(invoiceItemsList, invoiceHeader.getInvoiceNum());
+
+        Object[] results = JTableHelper.loadInvoiceItemsTable(invoiceLines);
+
+        defaultTableModel = (DefaultTableModel) results[1];
+        invoiceItemsTable = (JTable) results[0];
+        JScrollPane scrollable = new JScrollPane(invoiceItemsTable);
+        scrollable.setPreferredSize(new Dimension(400, 200));
+        tablePanel.add(scrollable);
+
+        saveButton = new JButton("Save");
+        saveButton.setActionCommand("S");
+        saveButton.addActionListener(new InvoiceLisener(this));
+
+        cancelButton = new JButton("Cancel");
+        cancelButton.setActionCommand("C");
+        cancelButton.addActionListener(new InvoiceLisener(this));
+
+        buttonsPanel.add(saveButton);
+        buttonsPanel.add(cancelButton);
+
+        setControlsValues(invoiceHeader);
+
+
+        rightPanel.add(infoPanel);
+        rightPanel.add(tablePanel);
+        rightPanel.add(buttonsPanel);
+
+    }
+
+
+    public void updateInvoiceTable(String filePath) {
+        this.invoiceFilePath = filePath;
+        fillInvoicesList();
+        DefaultTableModel tableModel = JTableHelper.constructInvoicesModel(this.invoicesList);
+        invoicesTable.setModel(tableModel);
+        invoicesTable.repaint();
+        updateRightPanel(0);
+
+    }
+
+    public void setControlsValues(InvoiceHeader invoiceHeader) {
+        if (invoiceHeader != null) {
+            invNumTF.setText(String.valueOf(invoiceHeader.getInvoiceNum()));
+            invDateTF.setText(invoiceHeader.getInvoiceDate().toString());
+            customerNmTF.setText(invoiceHeader.getCustomerName());
+            invTotalTF.setText(String.valueOf(invoiceHeader.getInvoiceTotal()));
+        } else {
+            invNumTF.setText("0");
+            invDateTF.setText("");
+            customerNmTF.setText("");
+            invTotalTF.setText("0.0");
+        }
+    }
+
+
+    public void updateRightPanel(int selectedIndex) {
+        if (selectedIndex == -1 || selectedIndex == -2) {
+            setControlsValues(null);
+            DefaultTableModel itemsModel = JTableHelper.constructInvoiceItemsTableModel(new ArrayList<>());
+            invoiceItemsTable.setModel(itemsModel);
+            invoiceItemsTable.repaint();
+            rightPanel.validate();
+            rightPanel.repaint();
+        } else if (invoicesList != null && invoicesList.size() != 0) {
+            InvoiceHeader invoiceHeader = invoicesList.get(selectedIndex);
+            setControlsValues(invoiceHeader);
+
+            ArrayList<InvoiceLine> invoiceLines = new InvoiceItemsHelper(invoiceItemsFilePath).getInvoiceItems(this.invoiceItemsList, invoiceHeader.getInvoiceNum());
+            DefaultTableModel itemsModel = JTableHelper.constructInvoiceItemsTableModel(invoiceLines);
+            invoiceItemsTable.setModel(itemsModel);
+            invoiceItemsTable.repaint();
+
+            rightPanel.validate();
+            rightPanel.repaint();
+        }
+    }
+}
